f02065fc59407bb97648dc00435a8b5f8166ca1a
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-16 12:34:37 -0300

    FileWriter

--- java-io/src/br/dev/rodrigocury/io/teste/TesteEscrita.java
+++ java-io/src/br/dev/rodrigocury/io/teste/TesteEscrita.java
@@ -2,19 +2,26 @@ package br.dev.rodrigocury.io.teste;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
 public class TesteEscrita {
 
 	public static void main(String[] args) throws IOException {
 		
-		writerTry();
+		fileWriter();
+		
+//		writerTry();
 //		
 //		// Fluxo de entrada com Arquivo
 //		
@@ -31,7 +38,7 @@ public class TesteEscrita {
 	
 	private static void writerTry() {
 		try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("txt.txt")))){
-			bw.write("Bixin é brabo\n");
+			bw.write("Bixin é brabo");
 			bw.newLine();
 			bw.write("Cehlokinho Bixo");
 		
@@ -41,5 +48,54 @@ public class TesteEscrita {
 		}
 	
 	}
+	
+	private static void writeList() {
+	    PrintWriter out = null;
+	    
+	    List<Integer> list = Arrays.asList(1,2,3,4,5,6,7);
+
+	    try {
+	        System.out.println("Entering" + " try statement");
+
+	        out = new PrintWriter(new FileWriter("OutFile.txt"));
+	        for (int i = 0; i < list.size(); i++) {
+	            out.println("Value at: " + i + " = " + list.get(i));
+	        }
+	    } catch (IndexOutOfBoundsException e) {
+	        System.err.println("Caught IndexOutOfBoundsException: "
+	                           +  e.getMessage());
+	                                 
+	    } catch (IOException e) {
+	        System.err.println("Caught IOException: " +  e.getMessage());
+	                                 
+	    } finally {
+	        if (out != null) {
+	            System.out.println("Closing PrintWriter");
+	            out.close();
+	        } 
+	        else {
+	            System.out.println("PrintWriter not open");
+	        }
+	    }
+	}
+	
+	private static void fileWriter() throws IOException {
+		File f = new File("txt3.txt");
+		System.out.println(File.pathSeparator);
+		System.out.println(File.separatorChar);
+
+		f.createNewFile();
+		try (
+				FileWriter fw = new FileWriter(f);
+			){
+			
+			fw.write("Teste");
+			fw.write(System.lineSeparator());
+			fw.write("Teste");
+			
+		} catch (IOException e){
+			System.out.println(e.getMessage());
+		}
+		}
 
 }
